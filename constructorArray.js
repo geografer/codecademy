e7fe58eff8765f6ef9f804ff3e1b92b00814639b
M. Adan <mawehelie@gmail.com>
2014-01-14 15:05:23 -0500

    constructors and arrays
    In this lesson, I am learning how to use constructors and then making
    arrays out of them.

---
+++ constructorArray.js
@@ -0,0 +1,14 @@
+// the animal constructor
+function Animal(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// An array of animals
+var species = new Array();
+species[0] = new Animal("alice", 40);
+species[1] = new Animal("bob", 42);
+species[2] = new Animal("michelle", 8);
+species[3] = new Animal("timmy", 6); 
+
+
